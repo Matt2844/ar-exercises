4a610276c085898cc908276be7b0ae54c6717212
Matt <matt.hd.ford@gmail.com>
2021-01-05 02:20:37 +0000

    done all

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,21 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+all = Stores.all.annual_revenue.sum
+puts all
+
+allRev = all / Stores.count
+puts allRev
+
+millPlus = Stores.where(annual_revenue: annual_revenue.size > '1000000')
+
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,28 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg:
+
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many = Store.employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to = Employee.store
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 6000)
+  @store2.employees.create(first_name: "Sam", last_name: "Smith", hourly_rate: 60)
+  @store3.employees.create(first_name: "Sally", last_name: "Smithy", hourly_rate: 65)
+  @store1.employees.create(first_name: "Sammy", last_name: "Smitty", hourly_rate: 30)
+  @store1.employees.create(first_name: "Bobby", last_name: "Johnson", hourly_rate: 40)
+
+end
+
+
